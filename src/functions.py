0e8933ba6b279d019c5d9e518883a5427887fcee
Illia Melnykov <fothingsill@gmail.com>
2022-12-02 17:11:16 +0100

    Added selection of tiles

--- src/functions.py
+++ src/functions.py
@@ -1,35 +1,75 @@
 import sys
 import pygame
 import variables
-from array import *
+import math
 
 
 # Start of program:
 def initGame():
     variables.window = pygame.display.set_mode((variables.WIDTH, variables.HEIGHT))
-    variables.window.fill(variables.bgColor)
+    variables.clock = pygame.time.Clock()
     drawGrid()
+    calculateTiles()
 
 
 # Call every frame (unity update analog)
 def tickGame():
+    variables.clock.tick(variables.FPS)
+    checkInputs()
+    checkPhysics()
+    checkDraw()
     pygame.display.update()
+
+def checkDraw():
+    drawGrid()
+    drawSelectedTile()
+
+
+
+def checkInputs():
+    checkExit()
+
+def checkPhysics():
+    pass
+
+
+# Check mouse position :|
+def drawSelectedTile():
+    mousePosition = pygame.mouse.get_pos()
+    closestTilePosition = findClosestTile(mousePosition)
+    selectedTile = pygame.Rect(closestTilePosition[0] - variables.TILE_SIZE / 2 + 1,
+                               closestTilePosition[1] - variables.TILE_SIZE / 2 + 1, variables.TILE_SIZE - 2,
+                               variables.TILE_SIZE - 2)
+    pygame.draw.rect(variables.window, variables.selectedTileColor, selectedTile, 30)
+
+
+# Find the closest tile to mouse position by iterating through array with tiles
+def findClosestTile(mousePos):
+    result = variables.tiles[0]
+    for coordinates in variables.tiles:
+        if math.dist(coordinates, mousePos) < math.dist(result, mousePos):
+            result = coordinates
+    return result
+
+
+# Checks for exit input
+def checkExit():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit(0)
 
 
-# Creates tiles
+# Creates tiles and writes tile centers to "variables.tiles"
 def drawGrid():
-    variables.tiles.append([])
-    iter_x = iter_y = 0
+    variables.window.fill(variables.bgColor)
     for x in range(0, variables.WIDTH, variables.TILE_SIZE):
         for y in range(0, variables.HEIGHT, variables.TILE_SIZE):
             rect = pygame.Rect(x, y, variables.TILE_SIZE, variables.TILE_SIZE)
             pygame.draw.rect(variables.window, variables.gridColor, rect, 1)
-            pygame.draw.circle(variables.window, (255,0,0), (x + variables.TILE_SIZE/2, y + variables.TILE_SIZE/2), 1, 1)
-            variables.tiles.append((x, y))
-            iter_y += 1
-        iter_y = 0
-        iter_x += 1
+
+
+def calculateTiles():
+    for x in range(0, variables.WIDTH, variables.TILE_SIZE):
+        for y in range(0, variables.HEIGHT, variables.TILE_SIZE):
+            variables.tiles.append((x + variables.TILE_SIZE / 2, y + variables.TILE_SIZE / 2))
--- src/variables.py
+++ src/variables.py
@@ -1,13 +1,19 @@
 import array
 
 # Window settings
+FPS = 60
 WIDTH, HEIGHT = 600, 600
+clock = None
 window = None
 
 # GameField
 TILE_SIZE = 40
-tiles = [[], []]
+tiles = []
+roadTiles = []
 
 # Colors
 bgColor = (60, 127, 60)
+selectedTileColor = (120, 240, 120)
+roadColor = (255, 0, 0)
 gridColor = (60, 60, 60)
+
